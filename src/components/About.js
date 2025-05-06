91e1743300889cce4545324edba60d6a7e689f39
Bo7mid3 <ahmedbahloul2526@gmail.com>
2025-05-06 14:45:17 +0100

    fix/fixed issue with async loading of data causing cannot read properties of undefined

--- src/components/About.js
+++ src/components/About.js
@@ -7,10 +7,10 @@ import React, { Component } from "react";
 class About extends Component {
   render() {
     if (this.props.resumeBasicInfo) {
-      var sectionName = this.props.resumeBasicInfo.section_name.about;
-      var hello = this.props.resumeBasicInfo.description_header;
-      var about = this.props.resumeBasicInfo.description;
-      var image = this.props.sharedBasicInfo.image;
+      var sectionName = this.props.resumeBasicInfo?.section_name.about;
+      var hello = this.props.resumeBasicInfo?.description_header;
+      var about = this.props.resumeBasicInfo?.description;
+      var image = this.props.sharedBasicInfo?.image;
     }
 
     return (
